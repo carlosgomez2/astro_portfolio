2dbad345e5d589bd3b5733275d40d0f81d3e349e
Carlos Gómez <carlosgomez2@users.noreply.github.com>
2023-05-11 19:18:51 -0600

    :construction: - testing how to load variables from markdown files

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    drafts: true,
+  },
   integrations: [tailwind()],
   server: { port: 8080, host: false},
   // server: { port: 8080, host: true},
