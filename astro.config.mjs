ba1aea74df9dc50693c194f652a76502062bd6c8
Carlos Gómez <carlosgomez2@users.noreply.github.com>
2024-04-14 19:16:33 -0600

    :arrow_up: - update project packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
       // Add custom languages
       // Note: Shiki has countless langs built-in, including .astro!
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: ['rb', 'py', 'js', 'perl', 'sql', 'shell', 'nginx', 'markdown', 'jinja-html', 'html', 'erb'],
+      langs: ['rb', 'py', 'js', 'perl', 'sql', 'shell', 'nginx', 'markdown', 'html', 'erb'],
       // Enable word wrap to prevent horizontal scrolling
       wrap: true,
     },
