030e412413aa53e6c505a5dcdbc0bb6a99f6a489
Carlos Gómez <carlosgomez2@users.noreply.github.com>
2024-04-14 18:48:43 -0600

    add articles

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,7 +11,18 @@ const workCollection = defineCollection({
     image: z.string().optional(),
   }),
 });
+const articlesCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    description: z.string(),
+    tags: z.array(z.string()),
+    slug: z.string(),
+    image: z.string().optional(),
+  }),
+});
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
   works: workCollection,
+  articles: articlesCollection,
 };
